655005e76827b8470322575fa48fb688b3e9d1ca
Taha <taha.hojati@gmail.com>
2016-01-26 15:42:26 -1000

    Added personal information from LinkedIn profile. TODO: write the display functions for each object.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,49 +1,70 @@
-bio {
-name : string
-role : string
-contacts : an object with
-      mobile: string
-      email: string 
-      github: string
-      twitter: string (optional)
-      location: string
-welcomeMessage: string 
-skills: array of strings
-biopic: url
-display: function
-}
+var bio = {
+    name: "Taha Pourjalali";
+    role: "Frond-end web developer";
+    contacts: {
+		mobile: "(808)232-8467";
+		email: "taha@hawaii.edu";
+		github: "https://github.com/tahahojati";
+	};
+    location: "Honolulu, HI";
+    welcomeMessage: "Physics graduate and experienced web developer.";
+    skills: ["Computer programming: C/C++, Java, Perl, Python",
+	"Web design: PHP, Javascript, HTML, CSS", "Knowledgable in undergraduate math and physics",
+	"Fluent in Persian with an understanding of Spanish"];
+    biopic: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAUfAAAAJGQ2ZjEwY2MwLWYwMDUtNDBlZS04ZTM5LWY3YzEwYWEzZWZmNA.jpg";
+    display: function
+};
 
-education {
-	schools: array of objects with
-     name: string
-     location: string
-     degree: string
-     majors: array of strings
-     dates: string (works with a hyphen between them)
-     url: string
-onlineCourses: array of objects with
-     title: string
-     school: string
-     date: string (works with a hyphen between them)
-     url: string
-display: function
-}
+var education = {
+    schools: [{
+			name: "University of Hawaii at Manoa";
+			location: "Honolulu, HI";
+			degree: "Bachelor of Science(B.S.)";
+			majors: ["Physics"];
+			dates: "2011 - 2016";
+			url: "www.hawaii.edu";
+	}]
+    onlineCourses: [{
+			title: "Front-End Web Developer Nanodegree";
+			school: "Udacity";
+			date: "2016";
+			url: "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001";
+	}]
+	display: function
+};
 
-work {
-	jobs: array of objects with
-     employer: string 
-     title: string 
-     location: string 
-     dates: string (Can be 'in progress')
-     description: string 
-display: function
-}
+var work = {
+    jobs: [{
+			employer: "Il Lupino Trattoria & Wine Bar";
+			title: "Waiter";
+			location: "Honolulu, HI";
+			dates: "October 2015 - Present";
+			description: "Serving delicious, authentic Italian cuisine made from highest quality ingredients in a relaxing atmosphere.";
+		},
+		{
+			employer: "The Old Spaghetti Factory";
+			title: "Waiter";
+			location: "Honolulu, HI";
+			dates: "June 2015 - October 2015";
+			description: "";
+		},
+		{
+			employer: "University of Hawaii at Manoa";
+			title: "Physics Tutor";
+			location: "Honolulu, HI";
+			dates: "May 2014 - August 2014";
+			description: "Tutored UH Manoa students in both mechanics and electricity and magnetism, and also conducted study sessions in both areas.";
+		}
+	]
+    display: function
+};
 
-projects {
-	projects: array of objects with
-      title: string 
-      dates: string (works with a hyphen between them)
-      description: string
-      images: array with string urls
-      display: function
-}
+var projects = {
+    projects: {[
+			title: "Constructing a 3D printer";
+			dates: "In progress";
+			description: "I am trying to build a 3D printer based on the well known reprap design.";
+			images: ["http://reprap.org/mediawiki/images/5/5b/Builder.jpg"];
+	}]
+    display: function
+};
