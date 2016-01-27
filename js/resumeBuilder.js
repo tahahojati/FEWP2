7c24d833cd258da757dad47ac1af21730ff03ff8
Taha <taha.hojati@gmail.com>
2016-01-26 14:53:48 -1000

    this commit wouldn't necessarily work!

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,49 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+bio {
+name : string
+role : string
+contacts : an object with
+      mobile: string
+      email: string 
+      github: string
+      twitter: string (optional)
+      location: string
+welcomeMessage: string 
+skills: array of strings
+biopic: url
+display: function
+}
+
+education {
+	schools: array of objects with
+     name: string
+     location: string
+     degree: string
+     majors: array of strings
+     dates: string (works with a hyphen between them)
+     url: string
+onlineCourses: array of objects with
+     title: string
+     school: string
+     date: string (works with a hyphen between them)
+     url: string
+display: function
+}
+
+work {
+	jobs: array of objects with
+     employer: string 
+     title: string 
+     location: string 
+     dates: string (Can be 'in progress')
+     description: string 
+display: function
+}
+
+projects {
+	projects: array of objects with
+      title: string 
+      dates: string (works with a hyphen between them)
+      description: string
+      images: array with string urls
+      display: function
+}
