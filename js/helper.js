a25d5c6523cc65dbf0c01be077c8b241f63f7d20
Taha <taha.hojati@gmail.com>
2016-01-27 06:23:33 -1000

    display function for bio was written

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,3 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
@@ -26,7 +12,7 @@ var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</sp
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box">%data%</ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
@@ -55,19 +41,7 @@ var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
-var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
-
-
-/*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
-$(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
-  });
-});
 
 /*
 The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,69 +2,112 @@ var bio = {
     name: "Taha Pourjalali",
     role: "Frond-end web developer",
     contacts: {
-		mobile: "(808)232-8467",
-		email: "taha@hawaii.edu",
-		github: "https://github.com/tahahojati",
-	},
+        mobile: "(808)232-8467",
+        email: "taha@hawaii.edu",
+        github: "https://github.com/tahahojati",
+    },
     location: "Honolulu, HI",
     welcomeMessage: "Physics graduate and experienced web developer.",
     skills: ["Computer programming: C/C++, Java, Perl, Python",
-	"Web design: PHP, Javascript, HTML, CSS", "Knowledgable in undergraduate math and physics",
-	"Fluent in Persian with an understanding of Spanish"],
+        "Web design: PHP, Javascript, HTML, CSS", "Knowledgable in undergraduate math and physics",
+        "Fluent in Persian with an understanding of Spanish"
+    ],
     biopic: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAUfAAAAJGQ2ZjEwY2MwLWYwMDUtNDBlZS04ZTM5LWY3YzEwYWEzZWZmNA.jpg",
-    //display: function
+    display: function() {
+        var lines = [];
+        lines[0] = HTMLheaderName.replace("%data%", bio.name);
+        lines[1] = HTMLheaderRole.replace("%data%", bio.role);
+        lines[2] = HTMLmobile.replace("%data%", bio.contacts.mobile);
+        lines[3] = HTMLemail.replace("%data%", bio.contacts.email);
+        lines[4] = HTMLgithub.replace("%data%", bio.contacts.github);
+        lines[5] = HTMLlocation.replace("%data%", bio.location);
+        lines[6] = HTMLbioPic.replace("%data%", bio.biopic);
+        lines[7] = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+        lines[8] = ""
+        for (var i in bio.skills) {
+            lines[8] = lines[8] + temp.replace("%data%", i) + "\n";
+        }
+        lines[8] = HTMLskillsStart.replace("%data%", lines[8]);
+        var output = "";
+        for (var i in lines) {
+            output = output + i + "\n";
+        }
+        return output;
+    }
 }
 
 var education = {
     schools: [{
-			name: "University of Hawaii at Manoa",
-			location: "Honolulu, HI",
-			degree: "Bachelor of Science(B.S.)",
-			majors: ["Physics"],
-			dates: "2011 - 2016",
-			url: "www.hawaii.edu",
-	}],
+        name: "University of Hawaii at Manoa",
+        location: "Honolulu, HI",
+        degree: "Bachelor of Science(B.S.)",
+        majors: ["Physics"],
+        dates: "2011 - 2016",
+        url: "www.hawaii.edu",
+    }],
     onlineCourses: [{
-			title: "Front-End Web Developer Nanodegree",
-			school: "Udacity",
-			date: "2016",
-			url: "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001",
-	}]
-	//display: function
+            title: "Front-End Web Developer Nanodegree",
+            school: "Udacity",
+            date: "2016",
+            url: "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001",
+        }]
+        //display: function
 }
 
 var work = {
     jobs: [{
-			employer: "Il Lupino Trattoria & Wine Bar",
-			title: "Waiter",
-			location: "Honolulu, HI",
-			dates: "October 2015 - Present",
-			description: "Serving delicious, authentic Italian cuisine made from highest quality ingredients in a relaxing atmosphere.",
-		},
-		{
-			employer: "The Old Spaghetti Factory",
-			title: "Waiter",
-			location: "Honolulu, HI",
-			dates: "June 2015 - October 2015",
-			description: "",
-		},
-		{
-			employer: "University of Hawaii at Manoa",
-			title: "Physics Tutor",
-			location: "Honolulu, HI",
-			dates: "May 2014 - August 2014",
-			description: "Tutored UH Manoa students in both mechanics and electricity and magnetism, and also conducted study sessions in both areas.",
-		}
-	]
-    //display: function
+            employer: "Il Lupino Trattoria & Wine Bar",
+            title: "Waiter",
+            location: "Honolulu, HI",
+            dates: "October 2015 - Present",
+            description: "Serving delicious, authentic Italian cuisine made from highest quality ingredients in a relaxing atmosphere.",
+        }, {
+            employer: "The Old Spaghetti Factory",
+            title: "Waiter",
+            location: "Honolulu, HI",
+            dates: "June 2015 - October 2015",
+            description: "",
+        }, {
+            employer: "University of Hawaii at Manoa",
+            title: "Physics Tutor",
+            location: "Honolulu, HI",
+            dates: "May 2014 - August 2014",
+            description: "Tutored UH Manoa students in both mechanics and electricity and magnetism, and also conducted study sessions in both areas.",
+        }]
+        //display: function
 }
 
 var projects = {
     projects: [{
-			title: "Constructing a 3D printer",
-			dates: "In progress",
-			description: "I am trying to build a 3D printer based on the well known reprap design.",
-			images: ["http://reprap.org/mediawiki/images/5/5b/Builder.jpg"],
-	}]
-    //display: function
+            title: "Constructing a 3D printer",
+            dates: "In progress",
+            description: "I am trying to build a 3D printer based on the well known reprap design.",
+            images: ["http://reprap.org/mediawiki/images/5/5b/Builder.jpg"],
+        }]
+        //display: function
 }
+
+/*HTMLheaderName = HTMLheaderName.replace("%data%", bio.name);
+HTMLheaderRole = HTMLheaderRole.replace("%data%", bio.role);
+//HTMLcontactGeneric = HTMLcontactGeneric.replace("%data%",'<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+HTMLmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+HTMLemail = HTMLemail.replace("%data%", bio.contacts.email);
+//HTMLtwitter = HTMLtwitter.replace("%data%",'<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+HTMLgithub = HTMLgithub.replace("%data%", bio.contacts.github);
+//HTMLblog = HTMLblog.replace("%data%",'<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+HTMLlocation = HTMLlocation.replace("%data%", bio.location);
+
+HTMLbioPic = HTMLbioPic.replace("%data%", bio.biopic);
+HTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+var temp = HTMLskills;
+HTMLskills = "";
+for (i in bio.skills) {
+    HTMLskills = HTMLskills + temp.replace("%data%", i) + "\n";
+}
+HTMLworkStart = HTMLworkStart.replace("%data%", '<div class="work-entry"></div>'; HTMLworkEmployer = HTMLworkEmployer.replace("%data%", '<a href="#">%data%'; HTMLworkTitle = HTMLworkTitle.replace("%data%", ' - %data%</a>'; HTMLworkDates = HTMLworkDates.replace("%data%", '<div class="date-text">%data%</div>'; HTMLworkLocation = HTMLworkLocation.replace("%data%", '<div class="location-text">%data%</div>'; HTMLworkDescription = HTMLworkDescription.replace("%data%", '<p><br>%data%</p>';
+
+                            HTMLprojectStart = HTMLprojectStart.replace("%data%", '<div class="project-entry"></div>'; HTMLprojectTitle = HTMLprojectTitle.replace("%data%", '<a href="#">%data%</a>'; HTMLprojectDates = HTMLprojectDates.replace("%data%", '<div class="date-text">%data%</div>'; HTMLprojectDescription = HTMLprojectDescription.replace("%data%", '<p><br>%data%</p>'; HTMLprojectImage = HTMLprojectImage.replace("%data%", '<img src="%data%">';
+
+                                                HTMLschoolStart = HTMLschoolStart.replace("%data%", '<div class="education-entry"></div>'; HTMLschoolName = HTMLschoolName.replace("%data%", '<a href="#">%data%'; HTMLschoolDegree = HTMLschoolDegree.replace("%data%", ' -- %data%</a>'; HTMLschoolDates = HTMLschoolDates.replace("%data%", '<div class="date-text">%data%</div>'; HTMLschoolLocation = HTMLschoolLocation.replace("%data%", '<div class="location-text">%data%</div>'; HTMLschoolMajor = HTMLschoolMajor.replace("%data%", '<em><br>Major: %data%</em>';
+
+                                                                        HTMLonlineClasses = HTMLonlineClasses.replace("%data%", '<h3>Online Classes</h3>'; HTMLonlineTitle = HTMLonlineTitle.replace("%data%", '<a href="#">%data%'; HTMLonlineSchool = HTMLonlineSchool.replace("%data%", ' - %data%</a>'; HTMLonlineDates = HTMLonlineDates.replace("%data%", '<div class="date-text">%data%</div>'; HTMLonlineURL = HTMLonlineURL.replace("%data%", '<br><a href="#">%data%</a>';*/
