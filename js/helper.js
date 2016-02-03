c3c6e6705d8d6d548c9c42c4d94e3f4067d2cc80
Taha <taha.hojati@gmail.com>
2016-02-02 14:50:28 -1000

    ready for another submission.

--- js/helper.js
+++ js/helper.js
@@ -100,7 +100,7 @@ function initializeMap() {
     var locations = [];
 
     // adds the single location property from bio to the locations array
-    locations.push(bio.location);
+    locations.push(bio.contacts.location);
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
